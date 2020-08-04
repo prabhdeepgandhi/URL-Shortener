d66e6b73789fd480ef7e66215923c8f9a1ac9703
Prabhdeep Singh Gandhi <30722742+prabhdeepgandhi@users.noreply.github.com>
2020-08-04 11:05:46 +0530

    Add files via upload
    Minor bug fixes and added View count

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ import json
 from werkzeug.utils import secure_filename
 import os.path
 
+
 app = Flask(__name__)
 app.secret_key='gdhasbvguvsduhuafhai'
+   
+
+def endcount():
+    if os.path.exists('count.json'):
+        with open('count.json') as count_file:
+            counts = json.load(count_file)
+
+    visit_count=counts["visits"]  
+    
+    counts={'visits': (visit_count+1)}
+    with open('count.json','w')as count_file :
+        json.dump(counts,count_file)
+        
 
 @app.route('/')
 def home():
+    endcount()
     return render_template('home.html')
 
 @app.route('/website_url')
@@ -19,9 +34,9 @@ def file_url():
     return render_template('file_url.html')
 
 
-
 @app.route('/your-url',methods=['GET','POST'])
 def your_url():
+   
     if request.method=='POST':
         urls={}
 
@@ -35,11 +50,15 @@ def your_url():
 
         if 'url' in request.form.keys():
             urls[request.form['code']]={'url':request.form['url']}
-        else:
+            
+        elif 'file' in request.form.keys():
             f= request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
             f.save('/home/prabhdeep/url-shortener/static/user_files/' + full_name)
             urls[request.form['code']]={'file':full_name}
+            
+        else :
+            return render_template('page_not_found.html'), 404
 
 
         with open('urls.json','w')as url_file :
@@ -47,7 +66,6 @@ def your_url():
         return render_template('your-url.html',code=request.form['code'])
     else:
         return redirect(url_for('home'))
-
 
 @app.route('/<string:code>')
 def redirect_to_url(code):
@@ -61,7 +79,10 @@ def redirect_to_url(code):
                         return redirect(url_for('static',filename='user_files/'+urls[code]['file']))
     return abort(404)
 
-
 @app.errorhandler(404)
 def page_not_found(error):
         return render_template('page_not_found.html'), 404
+
+if __name__ =="__main__" :
+    app.run(debug=False)
+
